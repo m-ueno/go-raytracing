25a48b32c167e71f00ee53f8494d2abfdf0ad938
m-ueno <m-ueno@users.noreply.github.com>
2017-09-30 02:31:01 +0900

    Add Scene

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
-	"math"
 	"strconv"
 )
 
@@ -16,7 +14,8 @@ func main() {
 	args := flag.Args()
 	size, _ := strconv.Atoi(args[0])
 
-	render(size)
+	scene := NewScene24(size)
+	scene.render()
 
 	log.Println("bye")
 }
@@ -25,72 +24,6 @@ type IntersectionPoint struct {
 	distance float64
 	position *Vector
 	normal   *Vector
-}
-
-func shadingAmbient(shape Shape) *FColor {
-	return shape.Material().k_a
-}
-
-func shadingDiffuse(ip *IntersectionPoint, lightSource LightSource, shape Shape) *FColor {
-	lighting := lightSource.LightingAt(ip.position)
-	k_d := shape.Material().k_d
-
-	v_n := ip.normal
-	v_l := Scale(-1.0, lighting.direction)
-	dot := math.Max(0, Dot(v_l, v_n))
-	r_d := FCScale(dot*lighting.intensity, k_d)
-
-	return r_d
-}
-
-func rayTrace(ray *Ray, shape *Sphere) *FColor {
-	/* 交差判定 */
-	ip, ok := shape.testIntersection(ray)
-
-	ls := NewPointLightSource(1, NewVector(-5, 5, -5))
-
-	fcolor := NewFColor(0, 0, 0)
-
-	if ok {
-		fcolor = FCAdd(fcolor, shadingAmbient(shape))
-		fcolor = FCAdd(fcolor, shadingDiffuse(ip, ls, shape))
-		log.Printf("ip:%v, fc:%v\n", ip, fcolor)
-
-	} else {
-		fcolor = NewFColor(100/255.0, 149/255.0, 237/255.0)
-	}
-
-	return fcolor
-}
-
-func render(size int) {
-	from := NewVector(0, 0, -5)
-
-	// Sphere
-	shape := NewSphere(
-		NewVector(0, 0, 5),
-		1.0,
-		NewMaterial(),
-	)
-	//	center := NewVector(0, 0, 5)
-	//	radius := 1.0
-
-	fmt.Printf("P3\n%d %d\n255\n", size, size)
-
-	for x := 0; x < size; x++ {
-		for y := 0; y < size; y++ {
-			screenXYZ := makeEye(x, y, size)
-
-			// 視線方向
-			to := Sub(screenXYZ, from)
-			to = Normalize(to)
-
-			ray := NewRay(from, to)
-
-			fcolor := rayTrace(ray, shape)
-			fmt.Print(fcolor)
-		}
-	}
 }
 
 func makeEye(x int, y int, imageSize int) *Vector {
---
+++ scene.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+)
+
+// 物体と光源の集合
+type Scene struct {
+	shapes           []Shape
+	lightSources     []LightSource
+	ambientIntensity float64
+	size             int
+}
+
+func NewScene24(size int) *Scene {
+	shapes := []Shape{
+		NewSphere(
+			NewVector(0, 0, 5),
+			1.0,
+			NewMaterial(),
+		),
+	}
+	lightSources := []LightSource{
+		NewPointLightSource(1.0, NewVector(-5, 5, -5)),
+	}
+	ambientIntensity := 0.1
+
+	return &Scene{
+		shapes:           shapes,
+		lightSources:     lightSources,
+		ambientIntensity: ambientIntensity,
+		size:             size,
+	}
+}
+
+func (sc *Scene) shadingAmbient(shape Shape) *FColor {
+	return FCScale(sc.ambientIntensity, shape.Material().k_a)
+}
+
+func (sc *Scene) shadingDiffuse(
+	ip *IntersectionPoint,
+	lightSource LightSource,
+	shape Shape) *FColor {
+	lighting := lightSource.LightingAt(ip.position)
+	k_d := shape.Material().k_d
+
+	v_n := ip.normal
+	v_l := Scale(-1.0, lighting.direction)
+	dot := math.Max(0, Dot(v_l, v_n))
+	r_d := FCScale(dot*lighting.intensity, k_d)
+
+	return r_d
+}
+
+func (sc *Scene) rayTrace(ray *Ray, shape Shape) *FColor {
+	/* 交差判定 */
+	ip, ok := shape.testIntersection(ray)
+
+	fcolor := sc.shadingAmbient(shape)
+
+	if ok {
+		for _, ls := range sc.lightSources {
+			fcolor = FCAdd(fcolor, sc.shadingDiffuse(ip, ls, shape))
+			log.Printf("ip:%v, fc:%v\n", ip, fcolor)
+		}
+	} else {
+		fcolor = NewFColor(100/255.0, 149/255.0, 237/255.0)
+	}
+
+	return fcolor
+}
+
+func (sc *Scene) render() {
+	size := sc.size
+	from := NewVector(0, 0, -5)
+
+	// Sphere
+	//	center := NewVector(0, 0, 5)
+	//	radius := 1.0
+
+	fmt.Printf("P3\n%d %d\n255\n", size, size)
+
+	for x := 0; x < size; x++ {
+		for y := 0; y < size; y++ {
+			screenXYZ := makeEye(x, y, size)
+
+			// 視線方向
+			to := Sub(screenXYZ, from)
+			to = Normalize(to)
+
+			ray := NewRay(from, to)
+
+			shape := sc.shapes[0]
+
+			fcolor := sc.rayTrace(ray, shape)
+			fmt.Print(fcolor)
+		}
+	}
+}
