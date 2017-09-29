1c0f36c94ddc30fcf45be92ec8fe133ff7082ee6
m-ueno <m-ueno@users.noreply.github.com>
2017-09-30 01:47:26 +0900

    Change color

--- material.go
+++ material.go
@@ -11,7 +11,7 @@ func NewMaterial() *Material {
 	return &Material{
 		shininess: 8,
 		k_a:       NewFColor(0.01, 0.01, 0.01),
-		k_d:       NewFColor(0.7, 0.7, 0.7),
+		k_d:       NewFColor(0.7, 0.5, 0.3),
 		k_s:       NewFColor(0.3, 0.3, 0.3),
 	}
 }
